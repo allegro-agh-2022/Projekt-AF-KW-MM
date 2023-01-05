dd9f5bd9fe7cee1a5013f789f86ac8c8967a7b8f
kerdamon <kwalas314@gmail.com>
2023-01-05 13:56:12 +0100

    Init

---
+++ auth/src/main/kotlin/AuthApplication.kt
@@ -0,0 +1,20 @@
+package auth 
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class DemoApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoApplication>(*args)
+}
+
+@RestController
+class MessageResource {
+    @GetMapping("/")
+    fun index(): String = "Hello world!"
+}
