b89761aa0d90db1fa8229d2c924186fc9f6df5eb
kerdamon <kwalas314@gmail.com>
2023-01-13 13:07:18 +0100

    Clean up

---
+++ service1/src/main/kotlin/Application.kt
@@ -0,0 +1,20 @@
+package service1 
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.data.annotation.Id
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
+
+@RestController
+class MessageResource {
+    @GetMapping("/")
+    fun index(): String = "Hello world!"
+}
