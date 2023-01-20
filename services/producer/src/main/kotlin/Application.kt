da5a5f7ee5d49fc8e1242b30d8ddd0e2aec7b277
kerdamon <kwalas314@gmail.com>
2023-01-20 21:40:07 +0000

    Add rabbitmq handling to producer

--- services/producer/src/main/kotlin/Application.kt
+++ services/producer/src/main/kotlin/Application.kt
@@ -4,7 +4,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.annotation.Id
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
 
 @SpringBootApplication
 class Application
@@ -14,13 +18,16 @@ fun main(args: Array<String>) {
 }
 
 @RestController
-class MessageResource {
+class MessageResource(val rabbitTemplate: RabbitTemplate) {
     @GetMapping("/")
     fun index(): String = "Hello world from producer index!"
 
     @GetMapping("/producer/")
     fun get1(): String = "Hello world form producer of path /producer/"
 
-    @GetMapping("/producer/something/")
-    fun get2(): String = "Hello world form producer of path /producer/something"
+    @PostMapping("/person/{name}")
+    fun postPerson(@PathVariable name: String): ResponseEntity<String> {
+        rabbitTemplate.convertAndSend("hello", name)
+        return ResponseEntity.ok(name)
+    }
 }
