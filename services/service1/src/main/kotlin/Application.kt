b30c0ba0b108d753448f8dc9f29156786eb8e0d0
kerdamon <kwalas314@gmail.com>
2023-01-20 21:50:49 +0000

    Add rabbitmq handling to service1 as consumer

--- services/service1/src/main/kotlin/Application.kt
+++ services/service1/src/main/kotlin/Application.kt
@@ -5,6 +5,9 @@ import org.springframework.boot.runApplication
 import org.springframework.data.annotation.Id
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.amqp.rabbit.annotation.RabbitHandler
+import org.springframework.amqp.rabbit.annotation.RabbitListener
+import org.springframework.stereotype.Service;
 
 @SpringBootApplication
 class Application
@@ -13,14 +16,11 @@ fun main(args: Array<String>) {
     runApplication<Application>(*args)
 }
 
-@RestController
-class MessageResource {
-    @GetMapping("/")
-    fun index(): String = "Hello world from service1 index"
-
-    @GetMapping("/service1/")
-    fun get1(): String = "Hello world form service1 of path /service1/"
-
-    @GetMapping("/service1/something/")
-    fun get2(): String = "Hello world form service1 of path /service1/something"
+@Service
+@RabbitListener(queues = ["hello"])
+class RabbitReceiver {
+    @RabbitHandler
+    fun receive(name: String) {
+        println("Received: '$name'")
+    }
 }
