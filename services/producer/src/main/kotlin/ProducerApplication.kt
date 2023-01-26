63e661788010cc69904c8773b57d03faef45388d
Adrian Furman <fadikk367@gmail.com>
2023-01-26 02:43:06 +0100

    Remove dummy consumer service

--- services/producer/src/main/kotlin/ProducerApplication.kt
+++ services/producer/src/main/kotlin/ProducerApplication.kt
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.annotation.Id
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.amqp.rabbit.core.RabbitTemplate
 import org.springframework.http.ResponseEntity
@@ -25,9 +24,15 @@ class HelloController() {
 
 @RestController
 class MessageResource(val rabbitTemplate: RabbitTemplate) {
-    @GetMapping("/producer/sendMessage/{name}")
-    fun sendMessageToHello(@PathVariable name: String): ResponseEntity<String> {
+    @GetMapping("/producer/order-status-changed/{name}")
+    fun emitOrderStatusChanged(@PathVariable name: String): ResponseEntity<String> {
         rabbitTemplate.convertAndSend("order_status_changed", name)
+        return ResponseEntity.ok(name)
+    }
+
+    @GetMapping("/producer/product-reviewed/{name}")
+    fun emitProductReviewed(@PathVariable name: String): ResponseEntity<String> {
+        rabbitTemplate.convertAndSend("product_reviewed", name)
         return ResponseEntity.ok(name)
     }
 }
